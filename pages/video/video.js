c85e79f6075e79c09103cd0f5d7b0a15fb8d11cc
Paul <819418426@qq.com>
2022-04-07 00:16:34 +0800

    视频页

--- pages/video/video.js
+++ pages/video/video.js
@@ -1,17 +1,49 @@
 // pages/video/video.js
+import request from '../../utils/request'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    videoGroupList: [],    //初始化导航标签数据
+    navId:'',   //导航标签的id标识
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 页面加载即请求导航标签
+    this.getVideoGroupList()
+  },
+
+  // 获取导航标签数据的功能函数
+  async getVideoGroupList() {
+    let result = await request('/video/group/list')
+    // 更新videoGroupList
+    this.setData({
+      videoGroupList:result.data.slice(0,14),
+      navId:result.data[0].id
+    })
+  },
+
+  // 点击下标，排他
+  selectTab(event){
+    // let navId = event.currentTarget.id    // 会自动将number转换成string
+    let navId = event.currentTarget.dataset.id    //
+    // 位移运算符，>>>0
+    // 目标数据线转换成二进制，然后去移动指定的位数，移出去的不要，不够的用零补齐
+    // 右移零位会强制转换数据类型为number
+    // 布尔值： !!!
+
+    // 修改navId的状态
+    this.setData({
+      navId
+    })
+    /* this.data.videoGroupList.forEach((item)=>{
+      item.selectTab = false
+    }) */
 
   },
 
